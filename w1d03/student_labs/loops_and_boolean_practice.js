dee3ad0f106dd1e620c16cf7a27ff8748b07b526
Kimber DePriest <66170878+KimberD22@users.noreply.github.com>
2020-06-01 23:42:07 -0400

    Add files via upload

---
+++ w1d03/student_labs/loops_and_boolean_practice.js
@@ -0,0 +1,47 @@
+//Afternoon lab Intro to Pseudo-Coding
+//EQUATOR DRIVE 
+//How much would it cost to drive around the world at he equator
+//create a calculator to get the cost 
+/*find equator miles or total distance of trip (24,000) 
+take total distance and divide by miles per gallon (15 mpg)
+this gives number of gallons needed 
+Multiply the number of gallons needed by gas prices per gallon
+*/
+
+//Hungry for More? Sandwich
+/* create variables for each sandwich element 
+variable = bread, peanut butter, jelly
+give each of the variables a number, perferably in order of assembly
+create an array
+print out array in order of assembly 
+bread
+peanut butter
+jelly 
+bread 
+if array prints in correct order 
+print out enjoy your sandwich */
+
+//Booleans & Loops Part 2
+ /*for (let faveDay = Saturday; faveDay = 6; faveDay > 6) faveDay++; {
+    console.log("I like the weekend");
+     if (faveDay < 5); faveDay++
+        console.log("Give me a good 'ol weekday");
+} 
+
+/*
+
+for (let sumNum = Math.floor((Math.random()) * 10) +1;
+sumNum <= 10; sumNum++) {
+    console.log("that's a small number");
+    if (sumNum > 20); sumNum++
+    console.log("this is a big number");
+    
+}
+ */
+
+//let faveDay = Saturday;
+for (let faveDay = 6; faveDay > 6; faveDay++) {
+    console.log("I like the weekend");
+}
+
+
